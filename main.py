13bdfc1fc6fc71968ebe62cfe77634fd617b83ab
l-Spook-l <uaspookua@gmail.com>
2025-05-31 23:03:06 +0300

    Created main and app files

---
+++ main.py
@@ -0,0 +1,17 @@
+
+
+"""
+при сборке - exe файла
+flet pack main.py --icon assets/icon.png
+"""
+
+def main():
+    pass
+
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Приложение остановлено")
