38b9d5dd6df448120a8a5807174639e743da69f3
l-Spook-l <uaspookua@gmail.com>
2025-06-01 20:25:01 +0300

    Launch file added to the main file

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
+import flet as ft
 
+from app import app
 
 """
 при сборке - exe файла
 flet pack main.py --icon assets/icon.png
 """
 
-def main():
-    pass
 
+def main():
+    ft.app(target=app)
 
 
 if __name__ == "__main__":
